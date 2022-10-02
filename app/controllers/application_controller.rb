9f3a4697c4a0702f7a2b6c1c74cfeeef76551915
leela khadka <khadkaleela3@gmail.com>
2022-10-02 15:53:21 -0700

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,41 @@ class ApplicationController < Sinatra::Base
     game = Game.find(params[:id])
 
     game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
+      reviews: { only: [:id, :comment, :score, :game_id, :user_id], include: {
+        user: { only: [:name, :id] }
       } }
     })
+  end
+
+  #delte review
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  #add reveiw
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  #Update
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
